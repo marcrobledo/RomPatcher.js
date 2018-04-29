c37c564d6c9aabad059b9abd89d599427f131d9f
Marc Robledo <mark@tinet.org>
2018-04-29 11:12:54 +0200

    added big favicon

--- _cache_service_worker.js
+++ _cache_service_worker.js
@@ -12,7 +12,7 @@ limitations under the License.
 mod by marcrobledo, original from: https://github.com/GoogleChrome/samples/blob/gh-pages/service-worker/basic/service-worker.js
 */
 
-const PRECACHE_ID='v20180428d';
+const PRECACHE_ID='v20180429';
 const PRECACHE_FILES=[
 'index.html','./',
 'RomPatcher.css',
