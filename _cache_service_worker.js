41d9af379f16f8f01ba2044175cd3de609ad2958
Marc Robledo <mark@tinet.org>
2020-07-12 17:41:27 +0200

    fix manifest+PWA icon

--- _cache_service_worker.js
+++ _cache_service_worker.js
@@ -29,7 +29,7 @@ caches.keys().then(function(cacheNames){
 });
 
 var PRECACHE_ID='rom-patcher-js';
-var PRECACHE_VERSION='v9';
+var PRECACHE_VERSION='v10';
 var PRECACHE_URLS=[
 	'/RomPatcher.js/','/RomPatcher.js/index.html',
 	'/RomPatcher.js/manifest.json',
