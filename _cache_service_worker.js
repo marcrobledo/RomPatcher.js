a4284790e0f61de308691eca5449c5452146ef41
Marc Robledo <mark@tinet.org>
2024-08-25 20:01:04 +0200

    version bump 3.0 RC4

--- _cache_service_worker.js
+++ _cache_service_worker.js
@@ -6,7 +6,7 @@
 */
 
 var PRECACHE_ID = 'rom-patcher-js';
-var PRECACHE_VERSION = 'v30rc3';
+var PRECACHE_VERSION = 'v30rc4';
 var PRECACHE_URLS = [
 	'/RomPatcher.js/', '/RomPatcher.js/index.html',
 	'/RomPatcher.js/manifest.json',
