8a34106d9d26b6eca736c0485d10aad42f872efa
Marc Robledo <mark@tinet.org>
2024-10-02 20:08:44 +0200

    fixed footer link to legacy version

--- _cache_service_worker.js
+++ _cache_service_worker.js
@@ -6,7 +6,7 @@
 */
 
 var PRECACHE_ID = 'rom-patcher-js';
-var PRECACHE_VERSION = 'v30final';
+var PRECACHE_VERSION = 'v30finalfix1';
 var PRECACHE_URLS = [
 	'/RomPatcher.js/', '/RomPatcher.js/index.html',
 	'/RomPatcher.js/manifest.json',
